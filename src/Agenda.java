19f1dfe0b18a79b214f539f562aa21dc8f02c31e
moniGitliz <moniliz.9012@gmail.com>
2025-05-16 10:39:46 -0500

    segundo commit, definiendo clases basicas

--- src/Agenda.java
+++ src/Agenda.java
@@ -1,2 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Agenda {
+    private List<Contacto> contacto = new ArrayList<>();
+
+
+    //------------- CONSTRUCTORES------------- //
+    public Agenda() {
+    }
+
+    public Agenda(ArrayList<Contacto> contacto) {
+        this.contacto = contacto;
+    }
+
+    //------------- GETTERS & SETTERS------------- //
+
+    public List<Contacto> getContacto() {
+        return contacto;
+    }
+
+    public void setContacto(List<Contacto> contacto) {
+        this.contacto = contacto;
+    }
+
 }
+
+
