f1b89c0e9351f179137134e0d50bffd1acd72fcc
Michaell Gonzalez Garcia <miagonzalezg@udistrital.edu.co>
2025-05-16 12:37:50 -0500

    metodo de listarContactos

--- src/Agenda.java
+++ src/Agenda.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Agenda {
@@ -21,6 +23,26 @@ public class Agenda {
 
     public void setContacto(List<Contacto> contacto) {
         this.contacto = contacto;
+    }
+
+
+    public void listarContactos() {
+        for (int i = 0; i < contacto.size() - 1; i++) {
+            for (int j = 0; j < contacto.size() - i - 1; j++) {
+                String nombre1 = contacto.get(j).getNombre();
+                String nombre2 = contacto.get(j + 1).getNombre();
+
+                if (nombre1.compareToIgnoreCase(nombre2) > 0) {
+                    Contacto reasig_nom = contacto.get(j);
+                    contacto.set(j, contacto.get(j + 1));
+                    contacto.set(j + 1, reasig_nom);
+                }
+            }
+        }
+        // Imprimir contactos ordenados
+        for (Contacto c : contacto) {
+            System.out.println(c.getNombre() + " " + c.getApellido() + " - " + c.getTelefono());
+        }
     }
 
 }
