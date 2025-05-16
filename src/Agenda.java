909b770f1c95e2113f3b5a3dbbc1a494428c7834
Maria Paula Morales <mariapaulamoralest13@gmail.com>
2025-05-16 11:45:45 -0600

    metodos

--- src/Agenda.java
+++ src/Agenda.java
@@ -3,6 +3,7 @@ import java.util.List;
 
 public class Agenda {
     private List<Contacto> contacto = new ArrayList<>();
+    private int maximo;
 
 
     //------------- CONSTRUCTORES------------- //
@@ -23,6 +24,30 @@ public class Agenda {
         this.contacto = contacto;
     }
 
+    public int getMaximo() {
+        return maximo;
+    }
+
+    public void setMaximo(int maximo) {
+        this.maximo = maximo;
+    }
+
+
+    public void buscaContacto(String nombreCompleto) {
+        for (Contacto contacto : contacto) {
+            String nombreYApellido = contacto.getNombre() + " " + contacto.getApellido();
+            if (nombreYApellido.equalsIgnoreCase(nombreCompleto.trim())) {
+                System.out.println("Teléfono: " + contacto.getTelefono());
+                return;
+            }
+        }
+        System.out.println("Contacto, no encontrado");
+    }
+
+    public void espacioLibres(){
+        int libres = maximo - contacto.size();
+        System.out.println("quedan" + " " + libres + " " + "espacios libres");
+    }
 }
 
 
